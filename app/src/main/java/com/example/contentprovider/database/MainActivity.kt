a5b1bfddf9053f9a9c841de8ecf2d87c16bbe034
Pablo-Rodrigues-git <pablo.rodrigues2015@outlook.com>
2021-08-26 00:52:48 -0300

    Created database Cursor, Created ContentProvider Base Columns, Created Loadermanager, created fun NoteClickeditem, created fun noteRemoveItem, created onCreateLoader, created onLoadFinished. Created adapter for NotesAdapter.kt

--- app/src/main/java/com/example/contentprovider/database/MainActivity.kt
+++ app/src/main/java/com/example/contentprovider/database/MainActivity.kt
@@ -1,12 +1,62 @@
 package com.example.contentprovider.database
 
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.BaseColumns._ID
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.contentprovider.R
+import com.example.contentprovider.database.NotesDatabaseHelper.Companion.TITTLE_NOTES
+import com.example.contentprovider.database.NotesProvider.Companion.URI_NOTES
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+abstract class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> {
+
+    lateinit var notesRecycleview: RecyclerView
+    lateinit var noteAdd: FloatingActionButton
+
+    lateinit var adapter: NotesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        noteAdd = findViewById(R.id.note_add)
+        noteAdd.setOnClickListener { }
+
+        adapter = NotesAdapter(object : NoteClickedListener{
+            override fun noteClickeditem(cursor: Cursor) {
+                val id = cursor.getLong(cursor.getColumnIndex(_ID))
+            }
+
+            override fun noteRemoveItem(cursor: Cursor?) {
+                val id = cursor?.getLong(cursor.getColumnIndex(_ID))
+                contentResolver.delete(Uri.withAppendedPath(URI_NOTES, id.toString()), null, null)
+            }
+
+        })
+        adapter.setHasStableIds(true)
+
+
+        notesRecycleview = findViewById(R.id.notes_recycler)
+        notesRecycleview.layoutManager = LinearLayoutManager(this)
+        notesRecycleview.adapter = adapter
+    }
+
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> =
+        CursorLoader(this, URI_NOTES, null, null, null, TITTLE_NOTES)
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        if (data != null) {  }
+
+        fun onLoaderReset(loader: Loader<Cursor>) {
+            TODO("Not yet implemented")
+        }
     }
 }
